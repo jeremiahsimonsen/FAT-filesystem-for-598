7dfffdda25f0336a17103f237e5ec9f2b34176f1
Jacob <jacoballenwood@gmail.com>
2016-04-27 11:51:39 -0400

    combined FAT file description with DirEntry file description, so both root and FAT will use the same structure which defines a file.

--- file_allocation_table.h
+++ file_allocation_table.h
@@ -3,34 +3,15 @@
  */
 
 
-
+/**
+ * @brief [This structure represents the entire File Allocation Table]
+ */
 typedef struct {
-
 	uint8_t FAT_ID;			// 8 bits in the first cluster (0) and is also copied into the BPB of the boot sector
-	short mount_flag;		// bit 15 if not set then was not properly unmounted before shutdown and could be dirty. should scan disk
-	short disk_flag;		// bit 14 if not set then disk I/O error on startup, possibly bad sectors. should run scan	
-	FILE_T * files;			// buffer of all the files in the FAT
-
+	uint16_t mount_flag;	// bit 15 if not set then was not properly unmounted before shutdown and could be dirty. should scan disk
+	uint16_t disk_flag;		// bit 14 if not set then disk I/O error on startup, possibly bad sectors. should run scan	
+	DirEntry * files;		// buffer of all the files in the FAT
 } FAT_T;
-
-
-
-typedef struct {
-
-	short * filename;		// name of the file
-	uint16_t parent_dir;	// points to starting cluster of parent dir ".."
-	CLUSTER_T * clusters;	// buffer of all the clusters in the file
-	uint16_t last_cluster;	// EOC end of chain which is last cluster in file
-
-} FILE_T;
-
-
-
-typedef struct {
-
-	uint16_t next_cluster;	// points to next cluster
-
-} CLUSTER_T;
 
 
 /**
@@ -41,25 +22,3 @@ typedef struct {
  * @return [description]
  */
 FAT_T * FAT(uint16_t address);
-
-
-/**
- * @brief [brief description]
- * @details [long description]
- * 
- * @param address [description]
- * @return [description]
- */
-FILE_T * FILE(uint16_t address);
-
-
-/**
- * @brief [brief description]
- * @details [long description]
- * 
- * @param address [description]
- * @return [description]
- */
-CLUSTER_T * CLUSTER(uint16_t address);
-
-
--- root_dir.h
+++ root_dir.h
@@ -8,19 +8,30 @@
 
 typedef struct {
 	uint8_t		filename[8];	///< Name of the file; 0th byte meaning:
-								///<   0x00: Unused
-								///<   0xe5: Deleted
-								///<   0x05: Actually starts with 0xe5
-								///<   0x2e: Directory
+								///< 	0x00: Unused
+								///< 	0xe5: Deleted
+								///< 	0x05: Actually starts with 0xe5
+								///< 	0x2e: Directory
 	uint8_t 	ext[3];	///< File extension; no special characters
 	uint8_t 	attributes;		///< File attributes
 	uint8_t 	reserved[10];	///< Special meaning - implementation-dependent
 	uint16_t 	modify_time;	///< Time created/last modified; format:
-								///<   hhhhhmmmmmmsssss (seconds/2)
+								///<   	hhhhhmmmmmmsssss (seconds/2)
 	uint16_t 	modify_date;	///< Data created/last modified; format:
-								///<   yyyyyyymmmmddddd (year 0 = 1980)
+								///<   	yyyyyyymmmmddddd (year 0 = 1980)
 	uint16_t 	start;			///< Start of file in clusters
 	uint32_t 	size;			///< File size in bytes
+
+	uint16_t    parent_dir;		///< points to starting cluster of parent dir ".."
+	uint16_t* 	clusters;		///< buffer of all the clusters in the file
+								///< cluster val:
+								///<	0x0000: 		free
+								///<	0x0002-ffef: 	cluster in use; 
+								///<					the value given is the number of the next cluster in the file
+								///<	0xfff0-fff6: 	reserved
+								///<	0xfff7: 		bad cluster
+								///<	0xfff8-ffff: 	cluster in use; 
+								///<					EOC end of chain which is last cluster in file
 } __attribute__((packed)) DirEntry;
 
 /**
